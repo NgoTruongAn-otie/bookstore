b8303c1151652d5828b22f28437b11f4ecf441d9
NgoTruongAn-otie <anngo210726@gmail.com>
2025-07-15 16:22:40 +0700

    update suggestion button

---
+++ script.js
@@ -0,0 +1,28 @@
+    document.addEventListener('DOMContentLoaded', function() {
+            // Xử lý dropdown mobile
+            const mobileDropdownTrigger = document.querySelector('.navbar-mobile-dropdown li:nth-child(2) > a');
+            const mobileDropdown = document.querySelector('.dropdown-mobile');
+            
+            if (mobileDropdownTrigger && mobileDropdown) {
+                mobileDropdownTrigger.addEventListener('click', function(e) {
+                    e.preventDefault();
+                    mobileDropdown.style.display = mobileDropdown.style.display === 'block' ? 'none' : 'block';
+                });
+            }
+        });
+
+    document.addEventListener("DOMContentLoaded", function () {
+            const toggleButton = document.getElementById("toggle-button");
+            const hiddenProducts = document.querySelectorAll(".hide-product");
+
+            let expanded = false;
+
+            toggleButton.addEventListener("click", function () {
+                hiddenProducts.forEach(item => {
+                    item.style.display = expanded ? "none" : "block";
+                });
+
+                toggleButton.textContent = expanded ? "Xem thêm" : "Thu gọn";
+                expanded = !expanded;
+            });
+        });
